fe9e68c5526ffc1149faabd33cd61685f6b7d124
pankajsinghtomar <74404454+pankajsinghtomar@users.noreply.github.com>
2023-09-18 15:53:46 +0000

    date and time

---
+++ 01_basics/date.js
@@ -0,0 +1,6 @@
+let myDate =  new Date()
+console.log(myDate.toString());
+console.log(myDate.toTimeString())
+console.log(myDate.toLocaleString())
+console.log(typeof myDate)
+let myCreatedDate = new Date ("10-04-2014")
